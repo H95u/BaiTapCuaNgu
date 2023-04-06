e89aa8b7e6a9b300587e801b340461f15810a25c
H95u <hieu.dh3t1@gmail.com>
2023-04-06 17:03:30 +0700

    nop bai tap

---
+++ src/Student.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Student {
+    String name;
+    int age;
+    double mathPoint;
+    double physicPoint;
+    double chemPoint;
+    static Scanner scanner = new Scanner(System.in);
+
+    public Student() {
+    }
+
+    public Student(String name, int age, double mathPoint, double physicPoint, double chemPoint) {
+        this.name = name;
+        this.age = age;
+        this.mathPoint = mathPoint;
+        this.physicPoint = physicPoint;
+        this.chemPoint = chemPoint;
+    }
+    public static Student createStudent(){
+        System.out.println("Nhập vào tên của hs :");
+        String name = scanner.nextLine();
+        System.out.println("Nhập vào tuổi của hs :");
+        int age = scanner.nextInt();
+        System.out.println("Nhập vào điểm toán :");
+        double mathPoint = scanner.nextDouble();
+        System.out.println("Nhập vào điểm lý :");
+        double physicPoint = scanner.nextDouble();
+        System.out.println("Nhập vào điểm hóa :");
+        double chemPoint = scanner.nextDouble();
+        scanner.nextLine();
+        return new Student(name,age,mathPoint,physicPoint,chemPoint);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMathPoint() {
+        return mathPoint;
+    }
+
+    public void setMathPoint(double mathPoint) {
+        this.mathPoint = mathPoint;
+    }
+
+    public double getPhysicPoint() {
+        return physicPoint;
+    }
+
+    public void setPhysicPoint(double physicPoint) {
+        this.physicPoint = physicPoint;
+    }
+
+    public double getChemPoint() {
+        return chemPoint;
+    }
+
+    public void setChemPoint(double chemPoint) {
+        this.chemPoint = chemPoint;
+    }
+
+    public double getAverage() {
+        return (mathPoint + chemPoint + physicPoint) / 3;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", mathPoint=" + mathPoint +
+                ", physicPoint=" + physicPoint +
+                ", chemPoint=" + chemPoint +
+                '}';
+    }
+}
---
+++ src/TestStudent.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số lượng hs :");
+        int size = scanner.nextInt();
+        Student[] studentArr = new Student[size];
+        for (int i = 0; i < studentArr.length; i++) {
+            System.out.println("Nhập vào hs thứ " + (i + 1));
+            studentArr[i] = Student.createStudent();
+        }
+        double max = studentArr[0].getAverage();
+        int index = 0;
+        for (int i = 0; i < studentArr.length; i++) {
+            if (studentArr[i].getAverage() > max) {
+                index = i;
+            }
+        }
+        System.out.println("Học sinh có điểm TBC cao nhất là " + studentArr[index].toString()
+                + "\nCó điểm TBC là :" + studentArr[index].getAverage());
+    }
+}
